b0be667490148056eb443db5d91774c8cb1d43ac
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-19 15:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// complete this js code
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+class Person{
+	constructor(name,age) {
+		this._name=name;
+		this._age=age;
+		// this._jobTitle=jobTitle;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+	}
+}
+
+class Employee extends Person {
+	constructor(name,age,jobTitle) {
+		super(name,age);
+		this._jobtitle=jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobtitle}`)
+	}
+	
+}
 
 // Do not change code below this line
 window.Person = Person;
